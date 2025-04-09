6462f3ea07926cc0828bd1cc5a7fdf105b417080
GUILHERME LUAN SILVA DOS REIS <guilherme.luan@a.ucb.br>
2025-04-09 10:42:11 -0300

    feat: add typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EstudanteModule } from './estudante/estudante.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [EstudanteModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: "localhost",
+      port: 3306,
+      username: "root",
+      password: "admin",
+      database: "test",
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    
+    
+    EstudanteModule],
   controllers: [AppController],
   providers: [AppService],
 })
