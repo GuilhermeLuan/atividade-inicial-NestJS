c1a413a15e0f50902df40dc05fca2c3d525e83be
GUILHERME LUAN SILVA DOS REIS <guilherme.luan@a.ucb.br>
2025-04-09 10:36:21 -0300

    feat: estudante entity e dto

---
+++ src/estudante/dto/create-estudante.dto.ts
@@ -0,0 +1,13 @@
+import { IsDateString, IsEmail, IsString } from "class-validator";
+
+export class CreateEstudanteDto {
+
+    @IsString()
+    nome: string;
+    @IsString()
+    matricula: string;
+    @IsEmail()
+    email: string;
+    @IsDateString()
+    dt_nascimento: string;
+}
---
+++ src/estudante/entities/estudante.entity.ts
@@ -0,0 +1,8 @@
+export class Estudante {
+    id: number;
+    nome: string;
+    matricula: string;
+    email: string;
+    dt_nascimento: Date;
+    // cidade_id: number;
+}
